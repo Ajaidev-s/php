5ba2eccadffa3675693bb4223e2d59747f2344c5
OBLIVIAN <146339839+ajwildr@users.noreply.github.com>
2023-11-30 14:10:49 +0530

    Add files via upload

---
+++ php/first.php
@@ -0,0 +1,24 @@
+</html>
+    <head>
+    	<title>first program</title>
+    </head>
+    <body>
+       <form name="first" method="POST">
+         <input type="text" name="name">
+         <input type="submit" style="color:red;" name="btn">
+       </form>
+       
+    </body>
+</html>  
+
+<?php>
+if(isset($_POST['btn'])
+  {
+  $n=$_POST['name'];
+  echo $n;
+  }
+?>  
+
+
+    
+
